b0978fcb440486eb41d9f132f698d152663ccb75
xihomararazo <xihomararazo@gmail.com>
2023-01-13 12:48:59 -0600

    Actividad 3

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,79 @@
-// Ejercicio 1
+var myPerson = {
+    name: "Xihomara",
+    age: 28,
+    profession: "Diseñador digital interactivo"
+};
+var getParametros = function (obj) { return Object.values(obj); };
+console.log(getParametros(myPerson));
 // Ejercicio 2
-// Ejercicio 3
-// Ejercicio 4
+function sumOrConcatenate(v1, v2) {
+    if (typeof v1 === "number" && typeof v2 === "number") {
+        return v1 + v2;
+    }
+    else if (typeof v1 === "string" && typeof v2 === "string") {
+        return v1 + "-" + v2;
+    }
+    else {
+        return v1 + ":" + v2;
+    }
+}
+console.log(sumOrConcatenate(4, 3));
+console.log(sumOrConcatenate(4, "hello"));
+console.log(sumOrConcatenate("hello", 4));
+console.log(sumOrConcatenate("hello", "world"));
+var myCar = {
+    tires: 4,
+    turnOnEngine: function () {
+        console.log("turnOnEngine");
+    },
+    pressPedal: function () {
+        console.log("pressPedal");
+    }
+};
+var myMotorcycle = {
+    tires: 4,
+    turnOnEngine: function () {
+        console.log("turnOnEngine");
+    },
+    openThrottle: function () {
+        console.log("openThrottle");
+    }
+};
+function isCar(obj) {
+    return obj.pressPedal !== undefined;
+}
+function acive(obj) {
+    obj.turnOnEngine();
+    if (isCar(obj)) {
+        obj.pressPedal();
+    }
+    else {
+        obj.openThrottle();
+    }
+}
+acive(myCar);
+acive(myMotorcycle);
+function removeFirstEntry(curentArray) {
+    return curentArray.slice(1, curentArray.length);
+}
+var strArray = [
+    "Hello",
+    "World",
+    "Im",
+    "a",
+    "Full",
+    "Stack",
+    "Developer",
+];
+var newStrArray = removeFirstEntry(strArray);
+console.log(newStrArray); //["World", "Im", "a", "Full", "Stack", "Developer"]
+var numArray = [1, 2, 3, 4, 5, 6, 7];
+var newNumArray = removeFirstEntry(numArray);
+console.log(newNumArray); //[2, 3, 4, 5, 6, 7]
+var mixedArray = ["Hello", "I", "have", 3, "tasks"];
+var newMixedArray = removeFirstEntry(mixedArray);
+console.log(newMixedArray); //["I", "have", 3, "tasks"]
+//const unsupportedArray = [{ name: "Lucas", surname: "Fernandez" }, "Hello", 22]; --> will fail
+// Argument of type '(string | number | { name: string; surname: string; })[]' is not assignable to parameter of type '(string | number)[]'.
+// Type 'string | number | { name: string; surname: string; }' is not assignable to type 'string | number'.
+// Type '{ name: string; surname: string; }' is not assignable to type 'string | number'.
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,7 +1,112 @@
 // Ejercicio 1
-
+interface Person {
+    name: string;
+    age: number;
+    profession: string;
+  }
+  const myPerson: Person = {
+    name: "Xihomara",
+    age: 28,
+    profession: "Diseñador digital interactivo",
+  };
+  type propiedades = string | number;
+  
+  let getParametros = (obj: Person): Array<propiedades> => Object.values(obj);
+  
+  console.log(getParametros(myPerson));
+  
 // Ejercicio 2
+function sumOrConcatenate(v1: string | number, v2: string | number) {
+  if (typeof v1 === "number" && typeof v2 === "number") {
+    return v1 + v2;
+  } else if (typeof v1 === "string" && typeof v2 === "string") {
+    return v1 + "-" + v2;
+  } else {
+    return v1 + ":" + v2;
+  }
+}
+console.log(sumOrConcatenate(4, 3));
+console.log(sumOrConcatenate(4, "hello"));
+console.log(sumOrConcatenate("hello", 4));
+console.log(sumOrConcatenate("hello", "world"));
 
 // Ejercicio 3
+interface Car {
+  tires: number;
+  turnOnEngine: () => void;
+  pressPedal: () => void;
+}
+interface Motorcycle {
+  tires: number;
+  turnOnEngine: () => void;
+  openThrottle: () => void;
+}
+const myCar: Car = {
+  tires: 4,
+  turnOnEngine: function () {
+    console.log("turnOnEngine");
+  },
+  pressPedal: function () {
+    console.log("pressPedal");
+  },
+};
+const myMotorcycle: Motorcycle = {
+  tires: 4,
+  turnOnEngine: function () {
+    console.log("turnOnEngine");
+  },
+  openThrottle: function () {
+    console.log("openThrottle");
+  },
+};
+
+type carOrMotorcycle = Car | Motorcycle;
+
+function isCar(obj: carOrMotorcycle): obj is Car {
+  return (obj as Car).pressPedal !== undefined;
+}
+function acive(obj: carOrMotorcycle) {
+  obj.turnOnEngine();
+  if (isCar(obj)) {
+    obj.pressPedal();
+  } else {
+    obj.openThrottle();
+  }
+}
+acive(myCar);
+acive(myMotorcycle);
 
 // Ejercicio 4
+type stringOrNumber = string | number;
+
+function removeFirstEntry<T extends keyof stringOrNumber | string | number>(
+  curentArray: Array<T>
+): Array<T> {
+  return curentArray.slice(1, curentArray.length);
+}
+
+const strArray: string[] = [
+  "Hello",
+  "World",
+  "Im",
+  "a",
+  "Full",
+  "Stack",
+  "Developer",
+];
+const newStrArray = removeFirstEntry(strArray);
+console.log(newStrArray); //["World", "Im", "a", "Full", "Stack", "Developer"]
+
+const numArray: number[] = [1, 2, 3, 4, 5, 6, 7];
+const newNumArray = removeFirstEntry(numArray);
+console.log(newNumArray); //[2, 3, 4, 5, 6, 7]
+
+const mixedArray: Array<number | string> = ["Hello", "I", "have", 3, "tasks"];
+const newMixedArray = removeFirstEntry(mixedArray);
+console.log(newMixedArray); //["I", "have", 3, "tasks"]
+
+//const unsupportedArray = [{ name: "Lucas", surname: "Fernandez" }, "Hello", 22]; --> will fail
+// Argument of type '(string | number | { name: string; surname: string; })[]' is not assignable to parameter of type '(string | number)[]'.
+// Type 'string | number | { name: string; surname: string; }' is not assignable to type 'string | number'.
+// Type '{ name: string; surname: string; }' is not assignable to type 'string | number'.
+
